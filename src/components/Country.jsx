2e39d3a4ca70d86e55e3f27bdcf0fe6f65481b2b
LiadLati <liadl57@gmail.com>
2024-12-29 00:46:17 +0200

    final static page

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from "react";
 
-const Country = () => {
+const Country = ({flag, name, population, region, capital}) => {
   return (
     // TODO: Country component
-    <div>Country</div>
-  )
-}
+    <>
+      <a href ="details.html"
+        className="country scale-effect countryDiv">
+        <div className="country-flag">
+          <img src={flag} alt={name}/>
+        </div>
+        <div className="country-info">
+          <h2 className="country-title">{name}</h2>
+          <ul className="country-brief">
+            <li><strong>population: </strong>{population}</li>
+            <li><strong>Region: </strong>{region}</li>
+            <li><strong>capital: </strong>{capital}</li>
+          </ul>
+        </div>
+      </a>
+    </>
+  );
+};
 
-export default Country
+export default Country;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Country from "../components/Country";
+import data from "../assets/CountriesData.json";
 
 const Home = () => {
-    return (
-        // TODO: Home page
-        // Render Country component (components/Country.jsx) for each country
-        // Take data from (assets/CountriesData.json)
-        <div>Home</div>
-    );
+  const [countries] = useState(data);
+  
+  return (
+    <>
+      <button
+        type="button"
+        aria-label="Scroll Top Button"
+        className="button scroll-top flex flex-ai-c flex-jc-c scale-effect"
+      >
+        <i className="fa-regular fa-chevron-up icon"></i>
+      </button>
+      <header className="header">
+        <div className="container flex flex-jc-sb flex-ai-c">
+          <div className="logo">
+            <a href="index.html">
+              <h1 className="headerh1">Where in the world?</h1>
+            </a>
+          </div>
+          <button
+            type="button"
+            aria-label="Theme Switcher Button"
+            className="theme-toggle flex flex-jc-sb flex-ai-c"
+          >
+            <span className="darkIcon">
+              <i className="fa-regular fa-moon theme-icon" id="darkButton"></i>
+            </span>
+            <span className="theme-text" id="darkText">
+              Dark Mode
+            </span>
+          </button>
+        </div>
+      </header>
+      <div className="body-container">
+        <section className="filters">
+          <div className="container">
+            <div className="search-wrapper">
+              <i className="fa-regular fa-magnifying-glass search-icon"></i>
+              <input
+                type="text"
+                className="search-input"
+                placeholder="Search for a country..."
+              />
+            </div>
+            <div className="dropdown-wrapper">
+              <div className="dropdown-header flex flex-jc-sb flex-ai-c">
+                <span>Filter by Region</span>
+                <i className="fa-regular fa-chevron-down icon"></i>
+              </div>
+              <div className="dropdown-body">
+                <ul>
+                  <li data-region="all">All</li>
+                  <li data-region="africa">africa</li>
+                  <li data-region="america">Americas</li>
+                  <li data-region="asia">Asia</li>
+                  <li data-region="europe">Europe</li>
+                  <li data-region="oceania">Oceania</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </section>
+        <main className="main">
+          <div className="container">
+            <section className="countries-grid">
+              {countries.map((country) => (
+                <Country
+                  key={country.name}
+                  flag={country.flag}
+                  name={country.name}
+                  population={country.population}
+                  region={country.region}
+                  capital={country.capital}
+                />
+              ))}
+            </section>
+          </div>
+        </main>
+      </div>
+    </>
+  );
 };
 
 export default Home;
